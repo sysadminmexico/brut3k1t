3d4241a9d0b106cc64e32e63978f9dbb8d2e759d
ex0dus-0x <ex0dus@codemuch.tech>
2017-04-18 16:18:38 -0400

    Fixed installer

--- extras/installer.py
+++ extras/installer.py
@@ -1,3 +1,15 @@
+from time import sleep
+import os, sys
+
+W = '\033[0m'  # white (normal)
+R = '\033[31m'  # red
+G = '\033[32m'  # green
+O = '\033[33m'  # orange
+B = '\033[34m'  # blue
+P = '\033[35m'  # purple
+C = '\033[36m'  # cyan
+GR = '\033[37m'  # gray
+
 def kali_build():
 
     print O + "Updating Repositories..." + W
